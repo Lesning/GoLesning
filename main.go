0e3823d74d4da22baeca0198b6d43ebb60a25d64
lesning <zwt0216@126.com>
2024-08-18 19:59:21 +0800

    Gin lesning learning

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main()  {
+	fmt.Println("hello")
+	r := gin.Default()
+	r.GET("/zhang",func (c *gin.Context)  {
+		c.JSON(http.StatusOK, gin.H{
+			"messg" : "ping",
+		})
+	})
+	r.Run(":8999")
+}
